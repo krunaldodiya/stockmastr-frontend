e83e894c88f245623d82348fb53a8e3d3c3c0e0e
krunaldodiya <kunal.dodiya1@gmail.com>
2018-10-24 17:31:45 +0530

    done

--- app/screens/guest/GetStartedScreen.js
+++ app/screens/guest/GetStartedScreen.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView, Image } from 'react-native';
 import {
-  View, Text, Button, Container, Form, Item, Content, Label,
+  View, Text, Button, Container, Form, Label, CheckBox,
 } from 'native-base';
 
-// components
-import { theme } from '../../libs/theme';
+const contract = require('../../../Assets/images/contract.png');
 
 const styles = StyleSheet.create({
   submitButton: {
     borderRadius: 20,
-    shadowColor: '#3f2201',
-    shadowOffset: { width: 3, height: 3 },
-    elevation: 3,
-    marginLeft: 5,
-    marginRight: 5,
     marginTop: 10,
-    marginBottom: 10,
     padding: 10,
-    backgroundColor: theme.background.primary,
+    backgroundColor: '#1B1B1B',
   },
 });
 
@@ -32,7 +25,9 @@ class GetStartedScreen extends React.Component {
 
     this.state = {
       buttonText: 'Agree & Continue',
-      termsText: 'lorem ipsum',
+      agree: false,
+      termsText:
+        '<h1>Dulce amarum, leve asperum, prope longe, stare movere, quadratum rotundum.</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non laboro, inquit, de nomine. Bonum incolumis acies: misera caecitas. Contineo me ab exemplis. Egone quaeris, inquit, quid sentiam? De illis, cum volemus. Duo Reges: constructio interrete. An tu me de L. </p><h5>Quid, de quo nulla dissensio est?</h5><p>Cur post Tarentum ad Archytam? Nam ista vestra: Si gravis, brevis; Quid est igitur, inquit, quod requiras? Sequitur disserendi ratio cognitioque naturae; </p><h1>Quod autem principium officii quaerunt, melius quam Pyrrho;</h1><p>Respondent extrema primis, media utrisque, omnia omnibus. Non semper, inquam; Quis istud, quaeso, nesciebat? Ita prorsus, inquam; Ne discipulum abducam, times. </p><p>Erat enim Polemonis. Praeclare hoc quidem. At multis malis affectus. Nulla erit controversia. </p><p>Sullae consulatum? Quod autem ratione actum est, id officium appellamus. Verum hoc idem saepe faciamus. Itaque his sapiens semper vacabit. </p><p>Obsecro, inquit, Torquate, haec dicit Epicurus? Cur post Tarentum ad Archytam? Honesta oratio, Socratica, Platonis etiam. At certe gravius. Restatis igitur vos; Quorum sine causa fieri nihil putandum est. Etenim semper illud extra est, quod arte comprehenditur. </p><h4>Dat enim intervalla et relaxat.</h4><p>Quae autem natura suae primae institutionis oblita est? Videsne, ut haec concinant? Sed quid attinet de rebus tam apertis plura requirere? Nosti, credo, illud: Nemo pius est, qui pietatem-; Quae cum dixisset paulumque institisset, Quid est? Nunc omni virtuti vitium contrario nomine opponitur. Facillimum id quidem est, inquam. </p><h3>Sed eum qui audiebant, quoad poterant, defendebant sententiam suam.</h3><p>Quid enim est a Chrysippo praetermissum in Stoicis? Cur igitur, inquam, res tam dissimiles eodem nomine appellas? Dat enim intervalla et relaxat. Idem adhuc; Id mihi magnum videtur. Sed residamus, inquit, si placet. </p><h6>Isto modo ne improbos quidem, si essent boni viri.</h6><p>Prioris generis est docilitas, memoria; Qua tu etiam inprudens utebare non numquam. Nullus est igitur cuiusquam dies natalis. Aliter enim explicari, quod quaeritur, non potest. Summae mihi videtur inscitiae. Est, ut dicis, inquit; </p><h2>Istam voluptatem perpetuam quis potest praestare sapienti?</h2><p>Quid ergo hoc loco intellegit honestum? Nemo igitur esse beatus potest. Si qua in iis corrigere voluit, deteriora fecit. Comprehensum, quod cognitum non habet? Nescio quo modo praetervolavit oratio. Sine ea igitur iucunde negat posse se vivere? Cum audissem Antiochum, Brute, ut solebam, cum M. Qualem igitur hominem natura inchoavit? </p>',
     };
   }
 
@@ -45,53 +40,59 @@ class GetStartedScreen extends React.Component {
         <Container
           style={{
             flex: 1,
-            paddingHorizontal: 10,
-            backgroundColor: theme.background.secondary,
+            padding: 24,
+            backgroundColor: '#fff',
             alignItems: 'center',
           }}
         >
           <Form>
             <View style={{ flex: 1 }}>
-              <Label
-                style={{
-                  fontWeight: 'bold',
-                  color: 'black',
-                  fontSize: 28,
-                  marginVertical: 20,
-                }}
-              >
-                Terms of service
-              </Label>
+              <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+                <Image style={{ width: 50, height: 50 }} source={contract} />
 
-              <Content bounces={false}>
-                <Text>
+                <Label
+                  style={{
+                    fontWeight: '500',
+                    color: 'black',
+                    fontSize: 22,
+                    paddingLeft: 12,
+                    paddingTop: 10,
+                  }}
+                >
+                  Terms & Conditions
+                </Label>
+              </View>
+
+              <ScrollView>
+                <Text style={{ fontSize: 12 }}>
                   {termsText}
                 </Text>
-              </Content>
+              </ScrollView>
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20, marginTop: 20 }}>
+              <CheckBox checked={this.state.agree} />
+              <Text
+                style={{
+                  marginLeft: 18,
+                  fontSize: 12,
+                }}
+              >
+                I have read all the terms & conditions.
+              </Text>
             </View>
 
             <View
               style={{
                 justifyContent: 'flex-end',
+                alignSelf: 'center',
               }}
             >
-              <Item
-                style={{
-                  marginBottom: 20,
-                  marginTop: 20,
-                  marginLeft: 10,
-                  marginRight: 10,
-                }}
-              >
-                <Button
-                  style={styles.submitButton}
-                  onPress={() => navigation.replace('OauthScreen')}
-                >
-                  <Text style={{ width: '100%', textAlign: 'center' }}>
-                    {buttonText}
-                  </Text>
-                </Button>
-              </Item>
+              <Button style={styles.submitButton} onPress={() => navigation.replace('OauthScreen')}>
+                <Text style={{ textAlign: 'center' }}>
+                  {buttonText}
+                </Text>
+              </Button>
             </View>
           </Form>
         </Container>
