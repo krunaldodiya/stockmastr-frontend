25869eaf154f8ee14b3d5e3763c61f7326732992
krunaldodiya <kunal.dodiya1@gmail.com>
2018-11-14 11:18:06 +0530

    done

--- app/screens/guest/VerifyOtpScreen.js
+++ app/screens/guest/VerifyOtpScreen.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React from "react";
 import {
-  View, Text, TouchableOpacity, Image, KeyboardAvoidingView, Alert,
-} from 'react-native';
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Alert
+} from "react-native";
 // 3rd
 // style
-import CodeInput from 'react-native-confirmation-code-input';
-import { compose, withApollo } from 'react-apollo';
-import Spinner from 'react-native-loading-spinner-overlay';
-import styles from '../../styles/VerifyOtpScreen';
+import CodeInput from "react-native-confirmation-code-input";
+import { compose, withApollo } from "react-apollo";
+import Spinner from "react-native-loading-spinner-overlay";
+import styles from "../../styles/VerifyOtpScreen";
 // services
 import {
-  getInitialScreen, checkUserExists, createUser, login, sendOtp,
-} from '../../services';
+  getInitialScreen,
+  checkUserExists,
+  createUser,
+  login,
+  sendOtp
+} from "../../services";
 // theme
-import theme from '../../libs/theme';
+import theme from "../../libs/theme";
 
 // images
-const phoneHand = require('../../../assets/images/phone-hand.png');
+const phoneHand = require("../../../assets/images/phone-hand.png");
 
 class VerifyOtpScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    header: null
   };
 
   constructor(props) {
@@ -34,7 +43,7 @@ class VerifyOtpScreen extends React.Component {
       otp: otp.toString(),
       verifyOtp: null,
       time: 20,
-      otpVerified: false,
+      otpVerified: false
     };
   }
 
@@ -49,7 +58,7 @@ class VerifyOtpScreen extends React.Component {
     }, 1000);
   }
 
-  onFinishCheckingCode = async (isValid) => {
+  onFinishCheckingCode = async isValid => {
     this.setState({ otpVerified: isValid });
   };
 
@@ -83,21 +92,23 @@ class VerifyOtpScreen extends React.Component {
       const { data } = await sendOtp(email);
 
       this.setState({ spinner: false, otp: data.otp.toString() });
-      return Alert.alert('Success', 'Please, check your email');
+      return Alert.alert("Success", "Please, check your email");
     } catch (error) {
       this.setState({ spinner: false });
     }
   };
 
   render() {
-    const {
-      otp, verifyOtp, time, otpVerified, spinner,
-    } = this.state;
+    const { email, otp, verifyOtp, time, otpVerified, spinner } = this.state;
 
     const { navigation } = this.props;
 
     return (
-      <KeyboardAvoidingView behavior="position" enabled style={styles.container}>
+      <KeyboardAvoidingView
+        behavior="position"
+        enabled
+        style={styles.container}
+      >
         <Spinner
           visible={spinner}
           textContent="Loading..."
@@ -107,8 +118,8 @@ class VerifyOtpScreen extends React.Component {
 
         <View
           style={{
-            alignItems: 'center',
-            marginTop: 20,
+            alignItems: "center",
+            marginTop: 20
           }}
         >
           <Image style={{ width: 130, height: 130 }} source={phoneHand} />
@@ -116,22 +127,22 @@ class VerifyOtpScreen extends React.Component {
 
         <View
           style={{
-            justifyContent: 'space-evenly',
-            alignItems: 'center',
-            marginTop: 20,
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            marginTop: 20
           }}
         >
           <View
             style={{
-              alignItems: 'center',
-              marginHorizontal: 15,
+              alignItems: "center",
+              marginHorizontal: 15
             }}
           >
             <Text
               style={{
                 fontSize: 16,
-                color: 'white',
-                fontFamily: theme.fonts.TitilliumWebRegular,
+                color: "white",
+                fontFamily: theme.fonts.TitilliumWebRegular
               }}
             >
               Please, Enter verification code send to &nbsp;
@@ -139,31 +150,31 @@ class VerifyOtpScreen extends React.Component {
             <Text
               style={{
                 fontSize: 16,
-                color: 'black',
-                fontFamily: theme.fonts.TitilliumWebSemiBold,
+                color: "black",
+                fontFamily: theme.fonts.TitilliumWebSemiBold
               }}
             >
-              john.doe@example.com
+              {email}
             </Text>
           </View>
 
           <View
             style={{
               borderWidth: 1,
-              borderColor: 'white',
+              borderColor: "white",
               borderRadius: 10,
               paddingVertical: 5,
               paddingHorizontal: 10,
-              marginTop: 20,
+              marginTop: 20
             }}
           >
-            <TouchableOpacity onPress={() => navigation.replace('OAuthScreen')}>
+            <TouchableOpacity onPress={() => navigation.replace("OAuthScreen")}>
               <Text
                 style={{
                   fontSize: 18,
-                  textAlign: 'center',
-                  color: '#ffffff',
-                  fontFamily: theme.fonts.TitilliumWebSemiBold,
+                  textAlign: "center",
+                  color: "#ffffff",
+                  fontFamily: theme.fonts.TitilliumWebSemiBold
                 }}
               >
                 Not your email ?
@@ -190,25 +201,28 @@ class VerifyOtpScreen extends React.Component {
 
         <View
           style={{
-            flexDirection: 'column',
-            marginTop: 10,
+            flexDirection: "column",
+            marginTop: 10
           }}
         >
-          <View style={{ alignItems: 'center' }}>
-            <TouchableOpacity disabled={time > 0} onPress={() => this.resendOtp()}>
+          <View style={{ alignItems: "center" }}>
+            <TouchableOpacity
+              disabled={time > 0}
+              onPress={() => this.resendOtp()}
+            >
               <Text
                 style={{
                   fontSize: 16,
-                  color: time > 0 ? 'gray' : 'indigo',
-                  fontFamily: theme.fonts.TitilliumWebRegular,
+                  color: time > 0 ? "gray" : "indigo",
+                  fontFamily: theme.fonts.TitilliumWebRegular
                 }}
               >
                 RESEND OTP &nbsp;
                 <Text
                   style={{
                     fontSize: 16,
-                    color: time > 0 ? 'gray' : 'black',
-                    fontFamily: theme.fonts.TitilliumWebRegular,
+                    color: time > 0 ? "gray" : "black",
+                    fontFamily: theme.fonts.TitilliumWebRegular
                   }}
                 >
                   IN
@@ -219,20 +233,20 @@ class VerifyOtpScreen extends React.Component {
             <View
               style={{
                 borderWidth: 2,
-                borderColor: 'white',
+                borderColor: "white",
                 borderRadius: 40,
                 marginTop: 20,
                 width: 60,
-                height: 60,
+                height: 60
               }}
             >
               <Text
                 style={{
-                  fontWeight: 'bold',
-                  color: 'white',
+                  fontWeight: "bold",
+                  color: "white",
                   fontSize: 22,
-                  textAlign: 'center',
-                  marginTop: 12,
+                  textAlign: "center",
+                  marginTop: 12
                 }}
               >
                 {time}
@@ -242,10 +256,18 @@ class VerifyOtpScreen extends React.Component {
 
           <View style={styles.submitButtonWrapper}>
             <TouchableOpacity
-              style={otpVerified ? styles.submitButton : styles.submitButtonDisabled}
+              style={
+                otpVerified ? styles.submitButton : styles.submitButtonDisabled
+              }
               onPress={() => this.otpAuth()}
             >
-              <Text style={otpVerified ? styles.submitButtonText : styles.submitButtonTextDisabled}>
+              <Text
+                style={
+                  otpVerified
+                    ? styles.submitButtonText
+                    : styles.submitButtonTextDisabled
+                }
+              >
                 VERIFY OTP
               </Text>
             </TouchableOpacity>
