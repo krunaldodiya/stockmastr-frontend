41bbc125c26a2f96a76e51b6272dc76e2ba3397e
krunaldodiya <kunal.dodiya1@gmail.com>
2018-11-21 17:38:50 +0530

    done

--- app/libs/vars.js
+++ app/libs/vars.js
@@ -1,8 +1,8 @@
 const httpUrlDev = "http://192.168.2.200:8000";
 const wsUrlDev = "ws://192.168.2.200:8000";
 
-const httpUrlProd = "https://socialstock-laravel.herokuapp.com";
-const wsUrlProd = "ws://socialstock-laravel.herokuapp.com";
+const httpUrlProd = "https://www.stockmastr.com";
+const wsUrlProd = "ws://www.stockmastr.com";
 
 const httpUrl = __DEV__ ? httpUrlDev : httpUrlProd;
 const wsUrl = __DEV__ ? wsUrlDev : wsUrlProd;
