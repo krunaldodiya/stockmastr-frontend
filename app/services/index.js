394ff11e4abb4e4210ebaa387625df9a713cdebb
krunaldodiya <kunal.dodiya1@gmail.com>
2018-11-21 17:29:37 +0530

    done

--- app/services/index.js
+++ app/services/index.js
@@ -6,13 +6,3 @@ export * from "./helpers";
 
 // api
 export * from "./api/graph";
-
-// graph
-export * from "./graph/check_user_exists";
-export * from "./graph/create_user";
-export * from "./graph/get_news";
-export * from "./graph/login";
-export * from "./graph/update_user";
-export * from "./graph/get_wallet";
-export * from "./graph/process_transaction";
-export * from "./graph/get_auth_user";
