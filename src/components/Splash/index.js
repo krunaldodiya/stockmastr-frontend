df22bc4545a55cbf7b2b12af475a0b290cc8dca8
krunaldodiya <kunal.dodiya1@gmail.com>
2018-12-05 17:13:09 +0530

    done

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./styles";
-import { Spinner, View } from "native-base";
+import { View, Image } from "react-native";
 
 const Splash = () => (
   <View style={styles.container}>
-    <Spinner color="#000" size="small" />
+    <Image
+      source={require("../../../assets/images/background.png")}
+      style={{ width: 100, height: 100 }}
+    />
   </View>
 );
 
